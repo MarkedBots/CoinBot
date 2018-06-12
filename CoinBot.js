578892302dd00f9359b9700954aac2c2fde532c5
MatthewSH <matthewh@halfpetal.com>
2018-06-12 18:50:04 -0400

    Give people coins

--- CoinBot.js
+++ CoinBot.js
@@ -12,21 +12,35 @@ exports.constructor = (api, helper) => {
     this.database.users.sync();
 
     setInterval(() => {
-      api.roster().then(roster => {
-        roster.members.forEach(member => {
-          this.database.users.findOrCreate({
-            where: {
-              id: member.userId
-            },
-            defaults: {
-              name: member.username
-            }
-          }).spread((user, created) => {});
+        api.roster().then(roster => {
+            roster.members.forEach(member => {
+                this.database.users.findOrCreate({
+                    where: {
+                        id: member.userId
+                    },
+                    defaults: {
+                        name: member.username
+                    }
+                }).spread((user, created) => {
+                    user.balance += 10;
+                    user.save().then(() => {});
+                });
+            });
         });
-      });
-    }, 10000);
+    }, 30000);
 };
 
 exports.balance = {
-
+    execute: (command, parameters, message) => {
+        this.database.users.findOrCreate({
+            where: {
+                id: message.userId
+            },
+            defaults: {
+                name: message.username
+            }
+        }).spread((user, created) => {
+            this.api.say("@" + message.username + "'s Balance: " + user.balance);
+        });
+    }
 };
