5f30c5f51788a55450276e7a8a7d2709d1386bcf
Matthew <matthew@pop-os.localdomain>
2018-06-12 09:28:11 -0400

    Adding database stuff for Sequelize.

--- CoinBot.js
+++ CoinBot.js
@@ -1,13 +1,28 @@
 exports.commands = ["balance"];
 
 let api;
-let helper; 
+let helper;
 let database;
 
 exports.constructor = (api, helper) => {
     this.api = api;
     this.helper = helper;
     this.database = require("./database");
+
+    this.database.users.sync();
+
+    api.roster().then(roster => {
+      roster.members.forEach(member => {
+        this.database.users.findOrCreate({
+          where: {
+            id: member.userId
+          },
+          defaults: {
+            name: member.username
+          }
+        }).spread((user, created) => {});
+      });
+    });
 };
 
 exports.balance = {
