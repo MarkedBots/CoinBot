a1acccc0fc04df9d71a9fcda08bf3115faf71e61
MatthewSH <matthewh@halfpetal.com>
2018-06-12 22:44:14 -0400

    Calling GTN from new import.

--- CoinBot.js
+++ CoinBot.js
@@ -1,5 +1,7 @@
 exports.commands = ["balance", "guessthenumber", "gtn"];
 
+const GuessTheNumber = require("./lib/GuessTheNumber");
+
 let api;
 let helper;
 let database;
@@ -49,43 +51,7 @@ exports.guessthenumber = {
     execute: (command, parameters, message) => {
         this.checkForUser(message.userId, message.username);
 
-        let coins = parameters[0];
-
-        if(coins < 10) {
-            this.api.say("You must be willing to lose at least 10 coins to play GTN.");
-            return;
-        }
-
-        this.database.users.findById(message.userId).then(user => {
-            if(user.balance < coins) {
-                this.api.say("@" + message.username + "...you do not have enough coins to GTN.");
-                return;
-            }
-        });
-
-        let guess = parameters[1];
-        let returnPercent = ((parameters[2] != "undefined") && (parameters[2] != null)) ? parameters[2] : 50;
-        let answer = Math.floor(Math.random() * (returnPercent - 0 + 1)) + 0;
-
-        if(guess == answer) {
-            let newCoins = Math.ceil((coins/100) * returnPercent);
-
-            this.database.users.findById(message.userId).then(user => {
-                user.balance += newCoins;
-                user.save().then(() => {
-                    this.api.say("The answer was " + answer + "! " +
-                                 message.username + " has won " + newCoins + ".")
-                });
-            });
-        } else {
-            this.database.users.findById(message.userId).then(user => {
-                user.balance -= coins;
-                user.save().then(() => {
-                    this.api.say("The answer was " + answer + "! " +
-                                 message.username + " has lost " + coins + ".")
-                });
-            });
-        }
+        GuessTheNumber.GTN(this.database, this.api, parameters, message);
     }
 }
 
