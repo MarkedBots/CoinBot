4d4f68f6f4700115927327a56f9d1a8406ea5d41
MatthewSH <matthewh@halfpetal.com>
2018-07-31 16:18:06 -0400

    More raid stuff.

--- src/lib/commands/user/Raid.ts
+++ src/lib/commands/user/Raid.ts
@@ -1,8 +1,15 @@
 import { Database } from "../../Database";
+import { Dungeon } from "../../helpers/Dungeon";
 
 export class Raid {
     private db: Database;
     private api: any;
+    private currentDungeonName: string = "";
+    private joining: boolean = false;
+    private started: boolean = false;
+    private inCooldown: boolean = false;
+    private cooldownLeft: number = 0;
+    private cooldownTime: number = 6e5; // 10m / 600s / 600,000ms.
 
     constructor(database: Database, api: any) {
         this.db = database;
@@ -10,6 +17,19 @@ export class Raid {
     }
 
     public execute(command: any, parameters: Array<string>, message: any): void {
-        this.api.say("Raid mode is being developed.");
+        this.currentDungeonName = Dungeon.generateName();
+        this.api.say("Starting raid for \"" + this.currentDungeonName + "\"");
+    }
+
+    private join(): boolean {
+        return true;
+    }
+
+    private start(): void {
+        //
+    }
+
+    private stop(): void {
+        //
     }
 }
