74bf2cef24614906b0b25139a14ad83ae7ea689d
MatthewSH <MatthewSH@users.github.com>
2018-08-05 06:28:09 -0400

    Adding transfering and taking.

--- src/CoinBot.ts
+++ src/CoinBot.ts
@@ -7,7 +7,8 @@ exports.commands = ["balance", "bal",
                     "guessthenumber", "gtn", 
                     "rockpaperscissors", "rps",
                     "admin",
-                    "raid"];
+                    "raid",
+                    "transfer"];
 
 let api;
 let helper;
@@ -78,6 +79,47 @@ exports.rps = {
     }
 };
 
+exports.transfer = {
+    execute: (command: any, parameters: any, message: any) => {
+        if (parameters[0] === undefined || parameters[1] === undefined) {
+            this.api.say("The give command must include a name AND an amount. Ex: !transfer username 10");
+        }
+
+        let username: string = parameters[0].toLowerCase();
+        let amount: number = Number(parameters[1]);
+        let userEntry: any = this.database.database().get("users").find({ name: username }).value();
+
+        if (amount === NaN || amount === undefined) {
+            this.api.say("The amount you gave is not a number.");
+            return;
+        }
+
+        if (amount < 1) {
+            this.api.say("You must transfer more than " + amount + " coins.");
+            return;
+        }
+
+        if (!this.database.users().hasCoins(message.userId, amount)) {
+            this.api.say("You do not have the coins to transfer, " + message.username + ".");
+            return;
+        }
+
+        if (userEntry === undefined) {
+            this.api.say(username + " does not exist in the CoinBot database.");
+            return;
+        }
+
+        let newCoins: number = <number>userEntry.coins + amount;
+
+        this.database.database().get("users").find({ name: username }).assign({ coins: newCoins }).write();
+        this.database.users().decrementCoin(message.userId, amount);
+
+        this.api.say("The coins have been transfered to the account. (" + message.username + " -> " + username + ")");
+
+        return;
+    }
+};
+
 exports.admin = {
     execute: (command: any, parameters: any, message: any) => {
         if (!this.adminCommand.isAdmin(message.username)) {
@@ -95,6 +137,16 @@ exports.admin = {
 
             this.adminCommand.give(username, amount);
             return;
-        }
+        } else if (parameters[0].toLowerCase() === "take") {
+            if (parameters[1] === undefined || parameters[2] === undefined) {
+                this.api.say("The take command must include a name AND an amount. Ex: !admin take username 10");
+            }
+
+            let username: string = parameters[1];
+            let amount: number = Number(parameters[2]);
+
+            this.adminCommand.take(username, amount);
+            return;
+        } 
     }
 };
--- src/lib/commands/management/Admin.ts
+++ src/lib/commands/management/Admin.ts
@@ -38,6 +38,39 @@ export class Admin {
         return;
     }
 
+    public take(username: string, amount: number): void {
+        username = username.toLowerCase();
+
+        let userEntry: any = this.db.database().get("users").find({ name: username }).value();
+
+        if (amount === NaN || amount === undefined) {
+            this.api.say("The amount you gave is not a number.");
+            return;
+        }
+
+        if (amount < 1) {
+            this.api.say("You must take more than " + amount + " coins.");
+            return;
+        }
+
+        if (userEntry === undefined) {
+            this.api.say(username + " does not exist in the CoinBot database.");
+            return;
+        }
+
+        let newCoins: number = <number>userEntry.coins - amount;
+
+        if (newCoins < 0) {
+            newCoins = 0;
+        }
+
+        this.db.database().get("users").find({ name: username }).assign({ coins: newCoins }).write();
+
+        this.api.say("The coins have been taken from the account (" + username + ")");
+
+        return;
+    }
+
     public isAdmin(username: string): boolean {
         return this.db.admins().indexOf(username) > -1;
     }
